71cdad7ff6d0fb6f3824003c8f71491795a1ea88
LenaDorfschmidt <lena.dorfschmidt@gmail.com>
2026-02-24 14:58:37 +0000

    more comments in python scripts

--- BCP-pipeline-testing/merge_synthseg_to_bops_labels.py
+++ BCP-pipeline-testing/merge_synthseg_to_bops_labels.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# This script merges SynthSeg segmentation labels to match the BOPS segmentation labels
+# Specifically, BOPS does not have cortical regions, but rather a global 'Left/right-Cerebral-Cortex' label
+#
 # merge_synthseg_to_bops_labels.py
 # Usage: python merge_synthseg_to_bops_labels.py input_synthseg.nii.gz output_bopslabels.nii.gz
 
@@ -11,6 +14,7 @@ def main(in_path, out_path):
     data = np.asarray(img.dataobj).astype(np.int32, copy=True)
 
     # Merge DK cortical labels -> BOPS cortical labels
+    # FreeSurfer LUT's use numbers over 1000 for left, and over 2000 for right cortical regions
     data[(data >= 1000) & (data <= 1999)] = 3   # Left-Cerebral-Cortex
     data[(data >= 2000) & (data <= 2999)] = 42  # Right-Cerebral-Cortex
 
@@ -18,6 +22,7 @@ def main(in_path, out_path):
     #drop_vals = [172, 77, 85, 30, 62, 31, 63]  # Vermis, WM-hypo, Optic-Chiasm, vessels, choroid plexus
     #data[np.isin(data, drop_vals)] = 0
 
+    # Save out results
     out_img = nb.Nifti1Image(data, img.affine, img.header)
     out_img.set_data_dtype(np.int32)
     nb.save(out_img, out_path)
